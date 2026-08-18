ca6279d2c2ca93c061f1f9796dc09d598c2f96af
Kailashkousik <kailashkoushik001@gmail.com>
2026-08-18 13:52:11 +0530

    Add files via upload

---
+++ Lab-1Code2.py
@@ -0,0 +1,10 @@
+def search(i):
+    l=[1001,1002,1003,1004,1005]
+    for j in range(len(l)):
+        if l[j]==i:
+            return j,i
+        else:
+            return("emp id not found")
+    return search(i)
+i = int(input("enter the id to be searched:"))      
+print(search(i))
